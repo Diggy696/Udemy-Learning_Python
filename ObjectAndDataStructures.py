7276fcf7f29469327e0ef766b0658e1ce38a0346
Darren <darren.webber.unc@gmail.com>
2020-12-30 16:34:26 -0600

    First File Upload from Udemy course

---
+++ ObjectAndDataStructures.py
@@ -0,0 +1,28 @@
+#integer + float
+print(3+3.3) #prints a float
+
+print('Hello World') # prints a string
+
+print(2+1)
+print(2-1)
+print(2*2)
+print(3/2)
+print(7//4) # floor division. actual value is 1.75- python pulls out just the 1.
+print(7%4) # remainder division - 4 goes into 7 once and a remainder of 3
+print(2**3) # exponential
+print(4**.5) # square root
+
+print( (2+10)* (10+3))
+
+
+# Variable assignment
+a=5
+print(a+a)
+a=10
+print(a)
+
+## Taxes
+income=53.44*2080
+taxrate=0.18
+mytotaltaxes=income*taxrate
+print(round(mytotaltaxes,2))
